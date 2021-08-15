666f2c3dde801c68a1e726abd52763ff2f57e0fa
julian-pulpo <julian.schrodinger@gmail.com>
2021-08-14 21:31:39 -0500

    añado tablas a agencia

--- agencia.sql
+++ agencia.sql
@@ -50,3 +50,56 @@ CREATE TABLE tblVuelos
     CONSTRAINT FK_ciudadDos_detblCiudad FOREIGN KEY(ciudadDos)
     REFERENCES tblCiudad(idCiudad)
 );
+
+-- TABLA HOTELES
+CREATE TABLE tblHoteles
+(
+    idHotel INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    -- FK
+    ciudad VARCHAR(5),
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    plazasDisponibles INT NOT NULL,
+    PRIMARY KEY(idHotel),
+    -- SE TRAEN FK
+    CONSTRAINT FK_ciudad_de_tblCiudad FOREIGN KEY(ciudad)
+    REFERENCES tblCiudad(idCiudad)
+);
+
+-- TABLA AGENCIA
+CREATE TABLE tblAgencia
+(
+    idAgencia INT AUTO_INCREMENT,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    PRIMARY KEY(idAgencia)
+);
+
+-- TABLA RESERVAS
+CREATE TABLE tblReservas
+(
+    idReserva INT AUTO_INCREMENT,
+    -- FK
+    cliente VARCHAR(20),
+    -- FK 
+    agencia INT,
+    -- FK
+    vuelo INT,
+    -- FK
+    hotel INT,
+    -- FK
+    hospedaje INT,
+    PRIMARY KEY(idReserva),
+    -- SE TRAEN FK
+    CONSTRAINT FK_cliente_tblCliente FOREIGN KEY(cliente)
+    REFERENCES tblClientes(idCliente),
+    CONSTRAINT FK_agencia_tblAgencia FOREIGN KEY(agencia)
+    REFERENCES tblAgencia(idAgencia),
+    CONSTRAINT FK_vuelo_tblVuelo FOREIGN KEY(vuelo)
+    REFERENCES tblVuelos(numeroVuelo),
+    CONSTRAINT FK_hotel_tblHotel FOREIGN KEY(hotel)
+    REFERENCES tblHoteles(idHotel),
+    CONSTRAINT FK_hospedaje_tblHospedaje FOREIGN KEY(hospedaje)
+    REFERENCES tblHospedaje(idHospedaje)
+);
